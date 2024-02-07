bf2da45e6210dcb0b6d840906fbae846ba35a140
theMomo93 <mkaminska93@gmail.com>
2024-02-07 19:24:51 +0100

    className

--- Final-Project/pages/index.js
+++ Final-Project/pages/index.js
@@ -16,17 +16,17 @@ const Home = () => {
 
         <main className="flex-1">
           <section className="">
-          <div class="flex  px-6 md:px-20  items-center justify-center bg-hero md:h-screen overflow-hidden">
-    <div class="flex flex-col  gap-6 md:flex-row items-center max-w-8xl">
-        <div class="w-full md:w-1/2 lg:pr-24">
-            <h2 class="text-4xl lg:text-5xl text-center md:text-left text-blue-900 leading-tight font-medium">There is no 
+          <div className="flex  px-6 md:px-20  items-center justify-center bg-hero md:h-screen overflow-hidden">
+    <div className="flex flex-col  gap-6 md:flex-row items-center max-w-8xl">
+        <div className="w-full md:w-1/2 lg:pr-24">
+            <h2 className="text-4xl lg:text-5xl text-center md:text-left text-blue-900 leading-tight font-medium">There is no 
                             better way to get information.</h2>
             <h3
-                class="mt-6 md:mt-10 text-md lg:text-xl text-center md:text-left text-gray-700 font-light tracking-wider leading-relaxed">
-                Port Germany is designed with immigrants in mind. Provide information and forum to
-                deliver helpful content right where your people need it, all in one place, all for You.
+                className="mt-6 md:mt-10 text-md lg:text-xl text-center md:text-left text-gray-700 font-light tracking-wider leading-relaxed">
+                <span className="font-semibold">Port Germany</span> is designed with immigrants in mind. Provide information and forum to
+                deliver helpful content right where people need it, all in one place, all for You.
             </h3>
-            <div class="mt-10 flex flex-col sm:flex-row justify-center md:justify-start">
+            <div className="mt-10 flex flex-col sm:flex-row justify-center md:justify-start">
                 <button className=" mb-2 md:mb-0 mr-0 md:mr-2 btn btn-intermediate">Register</button>
                 <button className="mb-2 md:mb-0 mr-0 md:mr-2 btn btn-intermediate">Login</button>
             </div>
