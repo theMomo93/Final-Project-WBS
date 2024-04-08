1cdcce28b70ba409858a62911e3ba99ca2c8a606
theMomo93 <mkaminska93@gmail.com>
2024-04-08 13:28:53 +0200

    _app.js lazy loading, progressbar

--- Final-Project/pages/_app.js
+++ Final-Project/pages/_app.js
@@ -1,13 +1,10 @@
-// pages/_app.js
 import React from 'react';
-import { AppProps } from 'next/app';
 import Router from 'next/router';
 import NProgress from 'nprogress'; // Import the nprogress library
 import 'nprogress/nprogress.css'; // Import the nprogress styles
 import Navbar from "../Components/Navbar";
 import '@/styles/globals.css';
 import { UserProvider } from "../contexts/UserContext.js";
-import { Poppins } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/Components/Footer";
 
@@ -16,16 +13,11 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-})
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <UserProvider>
-        <main className={poppins.className}>
+        <main>
           <Toaster position="top-center" />
           <Navbar />
           
