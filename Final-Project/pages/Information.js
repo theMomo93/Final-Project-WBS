f926fa44a5537fef172c55d783f1249885c45dd9
theMomo93 <mkaminska93@gmail.com>
2024-04-15 10:07:08 +0200

    infor page

--- Final-Project/pages/Information.js
+++ Final-Project/pages/Information.js
@@ -4,37 +4,34 @@ import { FiChevronsRight } from "react-icons/fi";
 import React from "react";
 import Footer from "@/Components/Footer";
 
-
 export default function Information() {
   const breadCrumbs = [
     { name: "Home", url: "/" },
     { name: "Information", url: "/Information" },
   ];
   return (
-  
-      <>
-        <BreadCrumbs breadCrumbs={breadCrumbs} />
-        <div className="mx-auto p-4 sm:p-12 h-auto px-2 sm:px-16 flex flex-wrap align-center justify-center ">
-          <div className=" mx-auto p-4 sm:p-12 h-auto px-2 sm:px-16 ">
-            <h1 className="text-4xl font-bold mx-4 sm:mx-28 mb-2 sm:mb-4 text-center">
-              Welcome, to our Information Page, choose a category:
-            </h1>
-    
-            <p className="text-lg mb-4 sm:mb-8 mx-4 sm:mx-52 text-center">
-              New to Germany and not quite sure how everything works? Our topics
-              may offer you useful information about living, working and staying
-              in Germany.
-            </p>
-          </div>
+    <>
+      <BreadCrumbs breadCrumbs={breadCrumbs} />
+      <div className="mx-auto p-4 sm:p-12 h-auto px-2 sm:px-16 flex flex-wrap align-center justify-center">
+        <div className="mx-auto p-4 sm:p-12 h-auto px-2 sm:px-16">
+          <h1 className="text-4xl font-bold mx-4 sm:mx-28 mb-2 sm:mb-4 text-center">
+            Welcome, to our Information Page, choose a category:
+          </h1>
 
-    
-        <section className="flex items-center lg:h-screen/2 font-poppins dark:bg-gray-50 pb-24 ">
+          <p className="text-lg mb-4 sm:mb-8 mx-4 sm:mx-52 text-center">
+            New to Germany and not quite sure how everything works? Our topics
+            may offer you useful information about living, working and staying
+            in Germany.
+          </p>
+        </div>
+
+        <section className="flex items-center lg:h-screen/2 font-poppins dark:bg-gray-50 pb-24">
           <div className="justify-center max-w-6xl px-4 py-4 mx-auto lg:py-0">
-            <div className="flex flex-wrap justify-center mx-3 pt-11 ">
-              <div className="w-full px-3 mb-6 lg:px-2 md:w-1/2 lg:w-1/2">
+            <div className="flex flex-wrap justify-center mx-3 pt-11">
+              <div className="w-full px-3 mb-6 lg:px-2">
                 <Link href="Information/Language">
-                  <div className="p-4 bg-white rounded-b shadow-md transition-transform transform hover:scale-105 hover:bg-blue-50 duration-300">
-                    <h2 className="text-3xl font-semibold text-black ">
+                  <a className="block p-4 bg-white rounded-b shadow-md transition-transform transform hover:scale-105 hover:bg-blue-50 duration-300">
+                    <h2 className="text-3xl font-semibold text-black">
                       Language Courses
                     </h2>
                     <p className="mt-2 text-l text-gray-700">
@@ -45,13 +42,13 @@ export default function Information() {
                       <span className="mr-2">Learn More</span>
                       <FiChevronsRight />
                     </div>
-                  </div>
+                  </a>
                 </Link>
               </div>
 
-              <div className="w-full px-3 mb-6 lg:px-2 md:w-1/2 lg:w-1/2">
+              <div className="w-full px-3 mb-6 lg:px-2">
                 <Link href="Information/Work">
-                  <div className="p-4 bg-white rounded-b shadow-md transition-transform transform hover:scale-105 hover:bg-blue-50 duration-300">
+                  <a className="block p-4 bg-white rounded-b shadow-md transition-transform transform hover:scale-105 hover:bg-blue-50 duration-300">
                     <h2 className="text-3xl font-semibold text-black">Work</h2>
                     <p className="mt-2 text-l text-gray-700">
                       Discover whether to look for jobs, your prospects and
@@ -61,13 +58,13 @@ export default function Information() {
                       <span className="mr-2">Learn More</span>
                       <FiChevronsRight />
                     </div>
-                  </div>
+                  </a>
                 </Link>
               </div>
 
-              <div className="w-full px-3 mb-6 lg:px-2 md:w-1/2 lg:w-1/2">
+              <div className="w-full px-3 mb-6 lg:px-2">
                 <Link href="Information/Healthcare">
-                  <div className="p-4 bg-white rounded-b shadow-md transition-transform transform hover:scale-105 hover:bg-blue-50 duration-300">
+                  <a className="block p-4 bg-white rounded-b shadow-md transition-transform transform hover:scale-105 hover:bg-blue-50 duration-300">
                     <h2 className="text-3xl font-semibold text-black">
                       Health Insurance
                     </h2>
@@ -79,13 +76,13 @@ export default function Information() {
                       <span className="mr-2">Learn More</span>
                       <FiChevronsRight />
                     </div>
-                  </div>
+                  </a>
                 </Link>
               </div>
 
-              <div className="w-full px-3 mb-6 lg:px-2 md:w-1/2 lg:w-1/2">
+              <div className="w-full px-3 mb-6 lg:px-2">
                 <Link href="Information/Housing">
-                  <div className="p-4 bg-white rounded-b shadow-md transition-transform transform hover:scale-105 hover:bg-blue-50 duration-300">
+                  <a className="block p-4 bg-white rounded-b shadow-md transition-transform transform hover:scale-105 hover:bg-blue-50 duration-300">
                     <h2 className="text-3xl font-semibold text-black">
                       Housing
                     </h2>
@@ -97,16 +94,14 @@ export default function Information() {
                       <span className="mr-2">Learn More</span>
                       <FiChevronsRight />
                     </div>
-                  </div>
+                  </a>
                 </Link>
               </div>
-
-             
             </div>
           </div>
         </section>
       </div>
-<Footer/>
+      <Footer />
     </>
   );
 }
